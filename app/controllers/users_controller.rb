7641efe8215baac03b35c50cb9a2ff3271aa5b17
pgtamatoa <pgpasturel@hotmail.com>
2015-05-28 19:15:18 +0200

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,12 +14,16 @@ class UsersController < ApplicationController
 	    @user = User.new(user_params)
 	    #debugger
 	    if @user.save
+	    	log_in @user
 	    	flash[:success] = "Welcome to the Sample App!"
 	    	redirect_to @user
 	      	# Handle a successful save.
-		else
+			else
 	      render 'new'
 	    end
+	end
+
+	def destroy
 	end
 
 	private

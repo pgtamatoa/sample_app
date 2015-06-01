7a850eee0a8a18c44cd5adbc98dc12d754c2eaf0
pgtamatoa <pgpasturel@hotmail.com>
2015-06-01 09:46:01 +0200

    Post index after review

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 content = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae aspernatur laudantium id aliquid? Quod et impedit error natus, ipsam, aspernatur dignissimos, nisi delectus asperiores doloribus repellendus veritatis ipsa. Deserunt, dolores.'
 user = User.create!(name: 'John', email: 'john@hellojam.fr', password: 'azerty')
-Post.create!(title: 'Article1', content: content, user_id: user.id)
-Post.create!(title: 'Article2', content: content, user_id: user.id)
-Post.create!(title: 'Article3', content: content, user_id: user.id)
-Post.create!(title: 'Article4', content: content, user_id: user.id)
+
+4.times do |i|
+  Post.create!(title: "Article #{i}", content: content, user_id: user.id)
+end
